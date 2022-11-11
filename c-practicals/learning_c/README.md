7aab47b19bf1bb4a91fb9ef4dd5fc75b37da7acf
MissJuly <joygitau98@gmail.com>
2022-11-11 23:13:44 +0300

    Card game

---
+++ c-practicals/learning_c/README.md
@@ -0,0 +1,5 @@
+# **learning_C**
+
+### **This file shows practice exercises in Head First C book**
+
+
---
+++ c-practicals/learning_c/in_class.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+	int c = 10;
+	
+	while (c > 10)
+	{
+		puts("I must not write code in class");
+		c = c - 1;
+	}
+	return (0);
+}
