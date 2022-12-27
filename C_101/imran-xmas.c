7d8068c1ccc8f0d6efa48faaca2bf67ddcef22c3
Barissa-Imran <imranbarissa@gmail.com>
2022-12-27 15:24:09 +0300

    Add xmas practical
    c syntax exercise

---
+++ C_101/imran-xmas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a chrismas tree on compile
+ * return: Always 0 success
+*/
+
+int main()
+{
+    printf("\t   ⁕\n");
+    printf("\t  ***\n");
+    printf("\t *****\n");
+    printf("\t*******\n");
+    printf("\t *****\n");
+
+    return (0);
+}
